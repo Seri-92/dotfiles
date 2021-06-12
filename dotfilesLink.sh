59e454b0f9f6367f3056751f6367dca1ca0a97a0
Seri-92 <64955102+serine-92@users.noreply.github.com>
2021-06-12 15:12:58 +0900

    ADD .tmux.conf

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,4 +4,5 @@ ln -sf ~/dotfiles/colors ~/.vim
 ln -sf ~/dotfiles/pythoh.vim ~/.vim/ftplugin
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 
